d8c156583cd89367dcba1da4750e56f81de2f33f
Jesus Pedrosa <jesus.pedrosa.guerrero@gmail.com>
2019-08-18 13:20:01 -0500

    feat(challenge): add structure for operations

---
+++ family_tree/resources/family.py
@@ -0,0 +1,20 @@
+
+
+def get_parents(person_id):
+    pass
+
+
+def get_cousins(person_id):
+    pass
+
+
+def get_grandparents(person_id):
+    pass
+
+
+def get_siblings(person_id):
+    pass
+
+
+def get_children(person_id):
+    pass
--- family_tree/views/health_check.py
+++ family_tree/views/health_check.py
@@ -1,5 +1,6 @@
 from flask import blueprints
 from flask import jsonify
+import family_tree.resources.family as res
 
 blueprint = blueprints.Blueprint('health_check', __name__)
 
@@ -7,3 +8,31 @@ blueprint = blueprints.Blueprint('health_check', __name__)
 @blueprint.route('/health_check', methods=['GET'])
 def health_check():
     return jsonify(True)
+
+
+@blueprint.route('/<person_id>/get_siblings', methods=['GET'])
+def get_siblings(person_id):
+    return jsonify(res.get_siblings(person_id))
+
+
+@blueprint.route('/<person_id>/get_parents', methods=['GET'])
+def get_parents(person_id):
+    return jsonify(res.get_parents(person_id))
+
+@blueprint.route('/<person_id>/get_children', methods=['GET'])
+def get_children(person_id):
+    return jsonify(res.get_children(person_id)) 
+
+
+@blueprint.route('/<person_id>/get_grandparents', methods=['GET'])
+def get_grandparents(person_id):
+    return jsonify(res.get_grandparents(person_id))
+
+
+@blueprint.route('/<person_id>/get_cousins', methods=['GET'])
+def get_cousins(person_id):
+    return jsonify(res.get_cousins(person_id))
+
+
+
+
