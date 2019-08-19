58669fa2dfd0dfcfda992a3eee53545e4753f2ad
Jesus Pedrosa <jesus.pedrosa.guerrero@gmail.com>
2019-08-18 21:18:51 -0500

    feat(challenge): add table creation script

---
+++ create_test_database.py
@@ -0,0 +1,18 @@
+from family_tree.models.family import Person, ParentChild
+from sqlalchemy import create_engine
+try:
+    import family_tree.config
+except ModuleNotFoundError:
+    print("ERROR: Please create a family_tree/config.py file with the USER and PASSWORD to use for database")
+    exit()
+user = config.USER
+password = config.PASSWORD
+
+conn_line = "postgres://{}:{}@localhost:5432".format(user, password)
+pg = create_engine(conn_line)
+
+
+with pg.connect() as conn:
+    Person.__table__.create(pg)
+    ParentChild.__table__.create(pg)
+
