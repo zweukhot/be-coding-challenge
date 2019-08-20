94ae814a5473427a20361f94d05a2ebe0f44970e
Jesus Pedrosa <jesus.pedrosa.guerrero@gmail.com>
2019-08-19 20:24:39 -0500

    feat(challenge): add constraints to tables

--- family_tree/models/family.py
+++ family_tree/models/family.py
@@ -1,11 +1,12 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, CheckConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import ForeignKey
+from sqlalchemy.orm import relationship, backref
 Base = declarative_base()
 
 class Person(Base):
     __tablename__ = 'person'
-    id = Column (Integer, primary_key=True)
+    id = Column (Integer, primary_key=True, autoincrement=True, unique=True)
     first_name = Column(String, primary_key=True)
     last_name = Column(String, primary_key=True)
     phone_number = Column(String)
@@ -16,9 +17,13 @@ class Person(Base):
 
 class ParentChild(Base):
     __tablename__ = 'parentchild'
-    id = Column(Integer, primary_key=True)
-    parent = Column(Integer, primary_key=True)
-    child = Column(Integer, primary_key=True)
+    __table_args__ = (CheckConstraint("parent <> child"),)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    parent = Column(Integer, ForeignKey("person.id", ondelete="CASCADE"), primary_key=True)
+    child = Column(Integer, ForeignKey("person.id", ondelete="CASCADE"), primary_key=True)
+    person = relationship("Persons", backref=backref("parentchild", cascade="all, delete-orphan", passive_deletes=True),)
+
+    
 
 
 
