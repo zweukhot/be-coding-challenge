9ec3fb7aba95157bedf84bec52c8928227d9fd7b
Jesus Pedrosa <jesus.pedrosa.guerrero@gmail.com>
2019-08-19 22:57:32 -0500

    feat(challenge): add final operations

--- family_tree/resources/family.py
+++ family_tree/resources/family.py
@@ -18,23 +18,33 @@ def get_parents(person_id):
     return person_to_list(query_results)
 
 def get_cousins(person_id):
-        
-    pass
+    grandpas = get_grandparents(person_id)
+    parents = {parent.id:parent
+                for grandpa in grandpas 
+                for parent in get_children(grandpa.id)}
+    grandchildren = {granchild.id:grandchild
+                     for parent in parents.values()
+                     for grandchild in fam_dm.get_children(parent.id)}
+    cousins = [grandchild for grandchild in grandchildren
+               if grandchild not in get_sibling(person_id)]
+    return cousins
 
 
 def get_grandparents(person_id):
     parents = fam_dm.get_parents(person_id)
+    #we enforce uniqueness
     grandpas = {grandpa.id: grandpa
                 for parent in parents
-                for granda in fam_dm.get_parents(parent)}
+                for grandpa in fam_dm.get_parents(parent.id)}
     return person_to_list(grandpas.values())
 
 
 def get_siblings(person_id):
     parents = fam_dm.get_parents(person_id)
+    # enforce uniqueness
     children = {child.id: child
                 for parent in parents
-                for child in fam_dm.get_children(parent)}
+                for child in fam_dm.get_children(parent.id)}
     return person_to_list(children.values())
 
 
