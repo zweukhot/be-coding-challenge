75ba416ef1f10c27b7059d4de457b4fe29dc25df
Jesus Pedrosa <jesus.pedrosa.guerrero@gmail.com>
2019-08-19 22:27:04 -0500

    feat(challenge): fix BIG mistakes in the queries

--- family_tree/datamodel/family.py
+++ family_tree/datamodel/family.py
@@ -3,26 +3,29 @@ from flask import current_app
 
 
 def get_person(first_name, last_name):
-    query = current_app.db.session.query(
-                                      Person).filter(
-                                              Person.last_name==last_name,
-                                              Person.first_name==first_name)
+    query = (current_app.db.session
+            .query(Person)
+            .filter(Person.last_name==last_name,
+                    Person.first_name==first_name)
     return query.all()
 
 
 def get_parents(person_id):
-    query = current_app.db.session.query(
-                                      ParentChild).filter(
-                                              ParentChild.child == person_id)
+    query = (current_app.db.session
+            .query(Person)
+            .filter(ParentChild.child_id == person_id,
+                    Person.id == ParentChild.parent_id)
+            )
     return query.all()
 
 
 def get_children(person_id):
-    query = current_app.db.session.query(
-                                      ParentChild).filter(
-                                              ParentChild.parent == person_id)
+    query = (current_app.db.session
+            .query(Person)
+            .filter(ParentChild.parent_id == person_id,
+                    Person.id == ParentChild.child_id)
+            )
     return query.all()
-
 
 
 
