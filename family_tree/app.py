62faa32647ef54766a6ce6b26b08c83574b7118c
Jesus Pedrosa <jesus.pedrosa.guerrero@gmail.com>
2019-08-20 00:43:21 -0500

    feat(challenge): fix less confusing urls

--- family_tree/app.py
+++ family_tree/app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from family_tree import config
 import flask_cors
 
-from family_tree.views import health_check
+from family_tree.views import family
 
 cors = flask_cors.CORS()
 
@@ -13,7 +13,7 @@ cors = flask_cors.CORS()
 def create_app():
     app = Flask(__name__)
     cors.init_app(app)
-    app.register_blueprint(health_check.blueprint, url_prefix='/api')
+    app.register_blueprint(family.blueprint, url_prefix='/api')
     database_file = "postgres://{}:{}@localhost:5432".format(config.USER, config.PASSWORD)
     app.config['SQLALCHEMY_DATABASE_URI'] = database_file
     db = SQLAlchemy(app)
--- family_tree/views/family.py
+++ family_tree/views/family.py
@@ -2,7 +2,7 @@ from flask import blueprints
 from flask import jsonify
 import family_tree.resources.family as res
 
-blueprint = blueprints.Blueprint('health_check', __name__)
+blueprint = blueprints.Blueprint('family', __name__)
 
 
 @blueprint.route('/health_check', methods=['GET'])
@@ -15,27 +15,27 @@ def search_person(last_name, first_name):
     return jsonify(res.get_person(first_name, last_name))
 
 
-@blueprint.route('/<person_id>/get_siblings', methods=['GET'])
+@blueprint.route('/get_siblings/<person_id>', methods=['GET'])
 def get_siblings(person_id):
     return jsonify(res.get_siblings(person_id))
 
 
-@blueprint.route('/<person_id>/get_parents', methods=['GET'])
+@blueprint.route('/get_parents/<person_id>', methods=['GET'])
 def get_parents(person_id):
     return jsonify(res.get_parents(person_id))
 
 
-@blueprint.route('/<person_id>/get_children', methods=['GET'])
+@blueprint.route('/get_children/<person_id>', methods=['GET'])
 def get_children(person_id):
     return jsonify(res.get_children(person_id)) 
 
 
-@blueprint.route('/<person_id>/get_grandparents', methods=['GET'])
+@blueprint.route('/get_grandparents/<person_id>', methods=['GET'])
 def get_grandparents(person_id):
     return jsonify(res.get_grandparents(person_id))
 
 
-@blueprint.route('/<person_id>/get_cousins', methods=['GET'])
+@blueprint.route('/get_cousins/<person_id>', methods=['GET'])
 def get_cousins(person_id):
     return jsonify(res.get_cousins(person_id))
 
